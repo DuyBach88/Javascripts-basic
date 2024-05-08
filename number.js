d69df119588b004eb87cf29ee2d10ee5201f9bb3
Easy to Code with Patric <bachbeo200003@gmail.com>
2024-05-09 00:02:37 +0700

    #1

---
+++ number.js
@@ -0,0 +1,11 @@
+console.log('hello duy bachs'.toUpperCase())
+let a,b,c;
+let d ='5';
+a=5;
+b= 123*1000;
+c=10;
+let e = +d
+let f = Number('5')
+console.log(a+b+c)
+console.log(a===f,typeof a,typeof f)
+console.log('evalue :' , e , 'type:',typeof e)
